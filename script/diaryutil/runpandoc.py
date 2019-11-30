85061d340358e6c8144b51044fe5a5900089fcb2
kb <d17179@mail.cmuh.org.tw>
2019-11-30 18:37:30 +0800

    scriptupdate

--- script/diaryutil/runpandoc.py
+++ script/diaryutil/runpandoc.py
@@ -5,3 +5,6 @@ def pandoc_html(filename):
 
 def pandoc_txt(filename):
     return subprocess.check_output(["pandoc", "-s", "-t", "plain", filename], encoding="utf-8")
+
+def pandoc_tex(filename):
+    return subprocess.check_output(["pandoc", "-s", "-t", "latex", filename], encoding="utf-8")
